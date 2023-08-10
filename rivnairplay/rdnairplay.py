a483310e330564b819e952fe36fa3a51f1773b0b
drew Roberts <zotzbro@gmail.com>
2023-08-10 11:09:47 -0400

    Update rdnairplay.py
    made a bunch of changes.
    
    read db login info from /etc/rd.conf
    get logmachine log from command line
    add logmachine code above to today's date to form running log name
    
    etc.

--- rivnairplay/rdnairplay.py
+++ rivnairplay/rdnairplay.py
@@ -4,16 +4,50 @@
 # right now, this is a dirty mashup of things / examples I have found on the web and put to alternative use.
 # this is moew to give you ideas and not for use.
 
+version="v0.01"
 
+import os
+from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
 import time
+import datetime
+import sys
 from sqlalchemy import create_engine
-my_conn = create_engine("mysql+mysqldb://rduser:rdusermysqlpasswd@localhost/Rivendell")  
+from sqlalchemy import text
+import configparser
 
 
+print("This is version: %s" % version)
 
+config = configparser.ConfigParser()
+config.read('/etc/rd.conf')
+dbuser = config['mySQL']['Loginname']
+dbpw = config['mySQL']['Password']
+#print("dbuser from config is: %s " % (dbuser))
+
+# get the database login info from /etc/rd.conf - more to do here.
+my_conn = create_engine("mysql+mysqldb://%s:%s@localhost/Rivendell" % (dbuser, dbpw)) 
+
+
+today = datetime.datetime.now()
+mydate = today.strftime('_%Y_%m_%d')
+#print(mydate)
+
+args = len(sys.argv) - 1
+#print ("The script was called with %i arguments" % (args))
+if(args > 0):
+    #print("The important parameter is %s." % (sys.argv[1]))
+    mybase = sys.argv[1]
+else:
+    mybase = "VID"
+
+mylog = mybase+mydate
+print("The log to deal with should be: %s" % mylog)
+#
+triggerfile = "/tmp/rivnan/current_"+mybase+".txt"
+#print("Trigger file is: ",triggerfile)
 
 def GetDateTime():
   # Get current date and time in ISO8601
@@ -27,7 +61,7 @@ def GetDateTime():
 
 def GetCartLine():
     # get info in file /tmp/rivnan/current_main.txt
-    with open('/tmp/rivnan/current_v101.txt') as f:
+    with open(triggerfile) as f:
         contents = f.read()
         fdata = contents.split(':')
         #print(contents)
@@ -48,40 +82,17 @@ class Application(tk.Frame):
 
     tk.Frame.__init__(self, master)
     #self.pack()
+    #self.grid(row=0)
     
     # Most of this stuff here is not needed and I need to remove it, but when I try, it affects other things
     # and I have not solved that issue yet.
 
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-             text='Local Time').grid(row=0, column=0)
-    self.LocalDate = tk.StringVar()
-    self.LocalDate.set('waiting...')
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-             textvariable=self.LocalDate).grid(row=0, column=1)
-
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-             text='Local Date').grid(row=1, column=0)
-    self.LocalTime = tk.StringVar()
-    self.LocalTime.set('waiting...')
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#be004e', fg = 'white', width = textwidth,
-             textvariable=self.LocalTime).grid(row=1, column=1)
-
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-             text='GMT Time').grid(row=2, column=0)
-    self.nowGdate = tk.StringVar()
-    self.nowGdate.set('waiting...')
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-             textvariable=self.nowGdate).grid(row=2, column=1)
-
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-             text='GMT Date').grid(row=3, column=0)
-    self.nowGtime = tk.StringVar()
-    self.nowGtime.set('waiting...')
-    tk.Label(self, font=('Helvetica', fontsize), bg = '#40CCC0', fg = 'white', width = textwidth,
-             textvariable=self.nowGtime).grid(row=3, column=1)
-
-    tk.Button(self, text='Exit', width = 10, bg = '#FF8080', command=root.destroy).grid(row=4, columnspan=2)
-    
+    #tk.Button(self, text='Exit', width = 10, bg = '#FF8080', command=root.destroy).grid(row=9, columnspan=2)
+    verlabl = tk.Label(master,
+             text="Rivendell new Airplay Version %s" % (version),
+             bg="white") 
+    #verlabl = "Version " + version
+    verlabl.pack()
     
     # define columns
     columns = ('1', '2', '3', '4', '5', '6', '7')
@@ -89,28 +100,47 @@ class Application(tk.Frame):
     # I don't like making tree global but I can't figure out what I am doing wrong.
     global tree
 
-    tree = ttk.Treeview(root, columns=columns, show='headings', height=40)
+    #tree = ttk.Treeview(root, columns=columns, show='headings', height=20)
+    tree = ttk.Treeview(self, columns=columns, show='headings', height=40)
+    #tv=ttk.Treeview(f,show='tree')
 
     # define headings
     # I really need to change the order of these columns throughout.
     # Also, there may be some I don't need and others I need to include.
     # Soon perhaps.
     tree.heading('1', text='Line_Id')
-    tree.heading('2', text='Count')
-    tree.heading('3', text='Start_Time')
-    tree.heading('4', text='Cart_Number')
-    tree.heading('5', text='Id')
-    tree.heading('6', text='Title')
-    tree.heading('7', text='Artist')
+    tree.heading('2', text='Cart_Number')
+    tree.heading('3', text='Title')
+    tree.heading('4', text='Artist')
+    tree.heading('5', text='Album')
+    tree.heading('6', text='Count')
+    tree.heading('7', text='Start_Time')
 
 
-    # generate sample data
-    #r_set = []
-    #for n in range(1, 100):
-    #    r_set.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+
+# below is an attempt to get log machine to watch in gui rather than command line
+#    c_set=my_conn.execute('''SELECT * FROM `LOG_MACHINES` WHERE `CURRENT_LOG` > '' ''')
+    
+#    a = [] #creates a list to store the job names in
+#    var = StringVar() #creates a stringvar to store the value of options
+    
+#    for c in c_set:
+#        a.append(c[6])
+    
+#    var.set(a[0]) #sets the default option of options
+    
+#    options = OptionMenu(root, var, *a) #createa an optionmenu populated with every element of the list
+#    button = Button(root, text="Ok", command=lambda:print(var.get())) #prints the current value of options
+    
+#    options.pack()
+#    button.pack()
+# here ends attempt to get log machine in interface
+
+
         
-    #r_set=my_conn.execute('''SELECT LINE_ID, COUNT, Start_Time, CART_NUMBER, ID from LOG_LINES WHERE LOG_NAME LIKE "AP1_2023_08_08"''')
-    r_set=my_conn.execute('''SELECT LOG_LINES.LINE_ID, LOG_LINES.COUNT, LOG_LINES.START_TIME, LOG_LINES.CART_NUMBER, LOG_LINES.ID, CART.TITLE, CART.ARTIST from LOG_LINES INNER JOIN CART WHERE LOG_LINES.CART_NUMBER = CART.NUMBER AND LOG_LINES.LOG_NAME LIKE "AP4_2023_08_09" ORDER BY LOG_LINES.LINE_ID ASC''')
+    stmt = text('''SELECT LOG_LINES.LINE_ID, LOG_LINES.CART_NUMBER, CART.TITLE, CART.ARTIST, CART.ALBUM, LOG_LINES.COUNT, LOG_LINES.START_TIME from LOG_LINES INNER JOIN CART WHERE LOG_LINES.CART_NUMBER = CART.NUMBER AND LOG_LINES.LOG_NAME LIKE :x ORDER BY LOG_LINES.LINE_ID ASC''')
+    stmt = stmt.bindparams(x=mylog)
+    r_set=my_conn.execute(stmt)
 
     # add data to the treeview
     for dt in r_set:
@@ -136,14 +166,19 @@ class Application(tk.Frame):
 
     tree.bind('<<TreeviewSelect>>', item_selected)
 
-    tree.grid(row=0, column=0, sticky='nsew')
+    #tree.grid(row=0, column=0, sticky='nsew')
+    #tree.pack(fill='x')
 
     # add a scrollbar
     scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=tree.yview)
     tree.configure(yscroll=scrollbar.set)
-    scrollbar.grid(row=0, column=1, sticky='ns')
+    #scrollbar.grid(row=0, column=1, sticky='ns')
+    scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
+    #ybar.pack(side=tk.RIGHT,fill=tk.Y)
+    tree.pack(fill='x')
+    self.pack()
 
-
+    tk.Button(self, text='Exit', width = 10, bg = '#FF8080', command=root.destroy).pack()
     self.gettime()
   pass
 
@@ -154,16 +189,18 @@ class Application(tk.Frame):
     gtm = gtm[0:2] + ':' + gtm[2:4] + ':' + gtm[4:6] + ' Z'  
     ldt = ldt[0:4] + '/' + ldt[4:6] + '/' + ldt[6:8]
     ltm = ltm[0:2] + ':' + ltm[2:4] + ':' + ltm[4:6]  
-    self.nowGtime.set(gdt)
-    self.nowGdate.set(gtm)
-    self.LocalTime.set(ldt)
-    self.LocalDate.set(ltm)
+    #self.nowGtime.set(gdt)
+    #self.nowGdate.set(gtm)
+    #self.LocalTime.set(ldt)
+    #self.LocalDate.set(ltm)
     myloginfo = GetCartLine()
     print("============================in gettime - Here comes myloginfo: ",myloginfo)
     zfcart = str(myloginfo[0])
     zflogline = str(myloginfo[1])
     tree.selection_set(zflogline)  # Does work.
-    tree.see(zflogline)
+    #tree.see(zflogline)
+    tree.see(int(zflogline)-10)
+    #tree.selection_set(zflogline)  # Does work
     # The below line means what is seen in the interface may lag the playout by 25 seconds.
     # For now, adjust to suit yourself.
     self.after(25000, self.gettime)
@@ -190,9 +227,8 @@ hs = root.winfo_screenheight() # height of the screen
 #x = (ws/2) - (w/2)
 #y = (hs/2) - (h/2)
 
-#right bottom corner (misfires in Win10 putting it too low. OK in Ubuntu)
 x = ws - w
-y = hs - h - 35  # -35 fixes it, more or less, for Win10
+y = hs - h
 
 #set the dimensions of the screen and where it is placed
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
